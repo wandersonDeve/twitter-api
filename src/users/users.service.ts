fad3ae00129040e16fd7735dd1e10ac9c6ddc877
wandersonDeve <Wanderlavo@outlook.com>
2021-10-04 20:57:31 -0300

    create prisma service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { User } from '.prisma/client';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private db: PrismaService) {}
+
+  async findUnique(username: string): Promise<User> {
+    const user = await this.db.user.findUnique({
+      where: { username },
+    });
+
+    if (!user) {
+      throw new NotFoundException();
+    }
+
+    return user;
+  }
+}
